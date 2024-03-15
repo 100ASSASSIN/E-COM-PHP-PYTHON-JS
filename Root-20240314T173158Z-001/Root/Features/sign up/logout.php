37c293596ca5c8fce2a7512a5d9e053dc9948d94
ASSASSIN <tamilthasan11@gamil.com>
2024-03-15 23:19:45 +0530

    Y

--- Root-20240314T173158Z-001/Root/Features/sign up/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Unset all cookies
-setcookie("username", "", time() - 3600, "/");
-setcookie("user_id", "", time() - 3600, "/");
-
-// Redirect to login page
-header("Location: login.php");
-exit();
-?>
--- Root-20240314T173158Z-001/Root/index.php
+++
@@ -1,5 +0,0 @@
-<?php
-include "Home.php";
-include "nav-bar.php";
-include "Footer.php";
-?>
