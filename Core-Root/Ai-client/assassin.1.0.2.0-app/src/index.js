d8693794fee26ae507af0a0c8722d6b7356d1e70
ASSASSIN <tamilthasan11@gamil.com>
2024-03-15 19:46:06 +0530

    r

--- Core-Root/Ai-client/assassin.1.0.2.0-app/src/index.js
+++ Core-Root/Ai-client/assassin.1.0.2.0-app/src/index.js
@@ -7,3 +7,6 @@ ReactDOM.render(
   <App />,
   document.getElementById('root')
 );
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+root.render(<p>Hello</p>);
