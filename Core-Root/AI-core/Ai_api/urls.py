6a1c1e1120746a36c6f3e5f81c0b00d93550e95e
ASSASSIN <tamilthasan11@gamil.com>
2024-03-17 00:29:43 +0530

    y

--- Core-Root/AI-core/Ai_api/urls.py
+++ Core-Root/AI-core/Ai_api/urls.py
@@ -3,4 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.api, name='api'),
+    path('page1/', views.page1, name='page1'),
+    path('page2/', views.page2, name='page2'),
+    path('page3/', views.page3, name='page3'),
+    path('page4/', views.page4, name='page4'),
+    path('page5/', views.page5, name='page5'),
+
 ]
--- Core-Root/AI-core/Ai_api/views.py
+++ Core-Root/AI-core/Ai_api/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from core.config import factorial 
-
+from django.urls import path
+from Ai_api import views
 # Assuming 'new' is a variable you want to include in your response
 new = "ASSASSIN"
 
@@ -14,3 +15,23 @@ def api(request):
 
     # Use JsonResponse to automatically convert your dictionary to JSON format
     return JsonResponse(data)
+
+def page1(request):
+    content = "This is Page 1"
+    return render(request, 'page.html', {'content': content})
+
+def page2(request):
+    content = "This is Page 2"
+    return render(request, 'app_name/page.html', {'content': content})
+
+def page3(request):
+    content = "This is Page 3"
+    return render(request, 'app_name/page.html', {'content': content})
+
+def page4(request):
+    content = "This is Page 4"
+    return render(request, 'app_name/page.html', {'content': content})
+
+def page5(request):
+    content = "This is Page 5"
+    return render(request, 'app_name/page.html', {'content': content})
