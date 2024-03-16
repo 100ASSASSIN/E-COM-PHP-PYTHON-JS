d59cd9635d6e05e306da6d4441dd48b2f4d4ef02
ASSASSIN <tamilthasan11@gamil.com>
2024-03-16 19:54:56 +0530

    TSE

--- Core-Root/AI-core/Ai_api/views.py
+++ Core-Root/AI-core/Ai_api/views.py
@@ -3,10 +3,10 @@ from django.http import JsonResponse
 from core import *
 # Assuming 'new' is a variable you want to include in your response
 new = "ASSASSIN"
-def core(request):
-    data = {"response": response}
+#def core(request):
+    #data = {"response": response}
 
-    return JsonResponse(response)
+    #return JsonResponse(response)
 def api(request):
     # Create a dictionary to be returned as JSON
     data = {"new": new}
