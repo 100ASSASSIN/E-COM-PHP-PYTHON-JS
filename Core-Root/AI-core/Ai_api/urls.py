a7b3c808b67c73018c275fbea5e174101bcde262
ASSASSIN <tamilthasan11@gamil.com>
2024-03-17 01:13:35 +0530

    y

--- Core-Root/AI-core/Ai_api/urls.py
+++ Core-Root/AI-core/Ai_api/urls.py
@@ -2,11 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.api, name='api'),
-    path('page1/', views.page1, name='page1'),
-    path('page2/', views.page2, name='page2'),
-    path('page3/', views.page3, name='page3'),
-    path('page4/', views.page4, name='page4'),
-    path('page5/', views.page5, name='page5'),
-
+    path('Ai_api/', views.members, name='members'),
 ]
--- Core-Root/AI-core/Ai_api/views.py
+++ Core-Root/AI-core/Ai_api/views.py
@@ -1,37 +1,7 @@
 from django.shortcuts import render
-from django.http import JsonResponse
-from core.config import factorial 
-from django.urls import path
-from Ai_api import views
-# Assuming 'new' is a variable you want to include in your response
-new = "ASSASSIN"
+from django.template import loader
+from django.http import HttpResponse
 
-def api(request):
-    # Calculate the factorial
-    result = factorial(5)
-
-    # Create a dictionary to be returned as JSON
-    data = {"new": new, "factorial_result": result}
-
-    # Use JsonResponse to automatically convert your dictionary to JSON format
-    return JsonResponse(data)
-
-def page1(request):
-    content = "This is Page 1"
-    return render(request, 'Ai_api/page.html', {'content': content})
-
-def page2(request):
-    content = "This is Page 2"
-    return render(request, 'app_name/page.html', {'content': content})
-
-def page3(request):
-    content = "This is Page 3"
-    return render(request, 'app_name/page.html', {'content': content})
-
-def page4(request):
-    content = "This is Page 4"
-    return render(request, 'app_name/page.html', {'content': content})
-
-def page5(request):
-    content = "This is Page 5"
-    return render(request, 'app_name/page.html', {'content': content})
+def members(request):
+    template = loader.get_template('page.html')
+    return HttpResponse(template.render({}, request))
